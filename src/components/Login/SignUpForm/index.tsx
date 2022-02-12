037f115ee9e18b0eccbc3914e53ca605efe4e2c9
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-12 14:36:42 -0300

    feat: import head para o title da page

--- src/components/Login/SignUpForm/index.tsx
+++ src/components/Login/SignUpForm/index.tsx
@@ -6,6 +6,7 @@ import {
   TiUserOutline,
 } from "react-icons/ti";
 
+import { PageHead } from "../../PageHead";
 import { Button } from "../../Button";
 import { Input } from "../../Input";
 
@@ -13,50 +14,56 @@ import "./styles.scss";
 
 export const SignUpForm = () => {
   return (
-    <div className="wrapper-signup float-left">
-      <form action="" className="signup-form">
-        <h2>Crie sua conta</h2>
-        <Input
-          type="text"
-          name="name"
-          id="name"
-          required
-          placeholder="Seu nome"
-          icon={<TiUserOutline />}
-        />
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          required
-          placeholder="Seu e-mail"
-          icon={<TiMail />}
-        />
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          required
-          placeholder="Sua senha"
-          icon={<TiLockClosedOutline />}
-        />
-        <Button type="submit" size="full" btnStyle="square">
-          Cadastrar
-        </Button>
-      </form>
+    <>
+      <PageHead
+        title="Cadastro | ticket"
+        description="Cadastre-se já para acompanhar os seus chamados."
+      />
+      <section className="wrapper-signup float-left">
+        <form action="" className="signup-form">
+          <h2>Crie sua conta</h2>
+          <Input
+            type="text"
+            name="name"
+            id="name"
+            required
+            placeholder="Seu nome"
+            icon={<TiUserOutline />}
+          />
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            required
+            placeholder="Seu e-mail"
+            icon={<TiMail />}
+          />
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            required
+            placeholder="Sua senha"
+            icon={<TiLockClosedOutline />}
+          />
+          <Button type="submit" size="full" btnStyle="square">
+            Cadastrar
+          </Button>
+        </form>
 
-      <div className="display-text">
-        <p>junte-se a nós!</p>
-        <h1>
-          Cadastre-se já para <br />
-          acompanhar os seus <br />
-          <span> chamados.</span>
-        </h1>
-        <Link to="/">
-          <TiArrowLeft />
-          Voltar para login
-        </Link>
-      </div>
-    </div>
+        <div className="display-text">
+          <p>junte-se a nós!</p>
+          <h1>
+            Cadastre-se já para <br />
+            acompanhar os seus <br />
+            <span> chamados.</span>
+          </h1>
+          <Link to="/">
+            <TiArrowLeft />
+            Voltar para login
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
