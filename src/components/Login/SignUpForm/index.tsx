08b21ffdc9905132e93ef872a69ae3ff7b2121ba
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-12 12:56:11 -0300

    feat: definindo a estrutura do form sign up

---
+++ src/components/Login/SignUpForm/index.tsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import {
+  TiArrowLeft,
+  TiLockClosedOutline,
+  TiMail,
+  TiUserOutline,
+} from "react-icons/ti";
+
+import { Button } from "../../Button";
+import { Input } from "../../Input";
+
+import "./styles.scss";
+
+export const SignUpForm = () => {
+  return (
+    <div className="wrapper-signup float-left">
+      <form action="" className="signup-form">
+        <h2>Crie sua conta</h2>
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          required
+          placeholder="Seu nome"
+          icon={<TiUserOutline />}
+        />
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          required
+          placeholder="Seu e-mail"
+          icon={<TiMail />}
+        />
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          required
+          placeholder="Sua senha"
+          icon={<TiLockClosedOutline />}
+        />
+        <Button type="submit" size="full" btnStyle="square">
+          Cadastrar
+        </Button>
+      </form>
+
+      <div className="display-text">
+        <p>junte-se a nós!</p>
+        <h1>
+          Cadastre-se já para <br />
+          acompanhar os seus <br />
+          <span> chamados.</span>
+        </h1>
+        <Link to="/">
+          <TiArrowLeft />
+          Voltar para login
+        </Link>
+      </div>
+    </div>
+  );
+};
