0a4562f46adca8662211b88af2a54ac3fb7a1923
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-12 08:03:01 -0300

    feat: definindo o diretório de pages

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import { LoginRoutes } from "../../routes/Login";
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <div>
+        <LoginRoutes />
+      </div>
+    </div>
+  );
+};
