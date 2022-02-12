26b5e6816b8f325a2e5223931cdb1efa1d2b2cbc
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-12 12:59:05 -0300

    feat: add footer e importando as rotas de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,19 @@
 import { LoginRoutes } from "../../routes/Login";
 
+import "./styles.scss";
+
 export const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-
-      <div>
+    <section className="wrapper-login">
+      <div className="container">
         <LoginRoutes />
       </div>
-    </div>
+      <footer className="login-footer">
+        <p>
+          &copy; 2022 &bull; <span>ticket.app</span> Todos os direitos
+          reservados
+        </p>
+      </footer>
+    </section>
   );
 };
