9581fed042cc5e9cfd378be3fb5b0446500cae28
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-17 16:04:13 -0300

    feat: definindo as rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import { AuthContextProvider } from "contexts/auth";
 
 import { Home } from "pages/Home";
 import { NotFound } from "pages/404";
@@ -7,16 +9,28 @@ import { SignInForm } from "components/Login/SignInForm";
 import { SignUpForm } from "components/Login/SignUpForm";
 
 import { Login } from "pages/Login";
+import PrivateRoute from "./Private";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />}>
-        <Route index element={<SignInForm />} />
-        <Route path="signup" element={<SignUpForm />} />
-      </Route>
-      <Route path="home" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route index element={<SignInForm />} />
+          <Route path="signup" element={<SignUpForm />} />
+        </Route>
+
+        <Route
+          path="home"
+          element={
+            <PrivateRoute redirectTo="/">
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AuthContextProvider>
   );
 };
