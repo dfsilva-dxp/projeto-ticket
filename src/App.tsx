70809218ff3f0a36cb838cf02c2e8be0a5ed6f45
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-17 16:06:50 -0300

    feat: removendo o context de auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { AuthContextProvider } from "contexts/auth";
 import { AppRoutes } from "routes";
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Router>
-        <AppRoutes />
-        <ToastContainer autoClose={3000} />
-      </Router>
-    </AuthContextProvider>
+    <Router>
+      <AppRoutes />
+      <ToastContainer autoClose={3000} />
+    </Router>
   );
 }
 
