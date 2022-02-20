fcfc179ed1c492692a7c908a915873ef4bb13674
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-20 10:07:20 -0300

    feat: refatorando o component layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,6 @@
+import Header from "components/Header";
 import { memo } from "react";
 import { Outlet } from "react-router-dom";
-
-import Header from "components/Header";
 
 import "./styles.scss";
 
@@ -10,8 +9,7 @@ const Layout = memo(() => {
   return (
     <section className="layout-grid">
       <Header />
-      <aside>Sidebar</aside>
-      <div className="content">
+      <div className="container">
         <Outlet />
       </div>
     </section>
