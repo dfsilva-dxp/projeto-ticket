764ec00c1b1f98a1d1ffff198dd68a897e2c27e6
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-20 10:06:24 -0300

    feat: refatorando o component avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -10,10 +10,6 @@ const Avatar = () => {
   return (
     <div className="avatar-wrapper">
       <div className="photo">{!!!user?.photoURL && <TiUserOutline />}</div>
-      <div className="avatar-info">
-        <h4>{user?.name}</h4>
-        <small>{user?.email}</small>
-      </div>
     </div>
   );
 };
