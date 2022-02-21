bd05e72f883592311f6bf7e3dd0df853e9d68342
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-21 18:57:10 -0300

    feat: ajustando o component avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,14 +1,18 @@
-import { TiUserOutline } from "react-icons/ti";
+import { TiPowerOutline, TiUserOutline } from "react-icons/ti";
 
 import useAuth from "hooks/useAuth";
 
 import "./styles.scss";
+import { Link } from "react-router-dom";
 
 const Avatar = () => {
   const { user } = useAuth();
 
   return (
     <div className="avatar-wrapper">
+      <Link to="/">
+        <TiPowerOutline />
+      </Link>
       <div className="photo">{!!!user?.photoURL && <TiUserOutline />}</div>
     </div>
   );
