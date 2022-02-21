136872f9e7b2e42b5a24d936f0dd354594ef1946
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-21 18:57:32 -0300

    feat: ajustando o component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,18 +8,25 @@ import { route } from "constants/routes";
 const Header = () => {
   return (
     <div className="header">
-      <Logo />
-      <nav>
-        <ul>
-          <li>
-            <NavLink to={`/${route.APP.HOME}`}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={`/${route.APP.CUSTOMERS}`}>Clientes</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Avatar />
+      <div className="container grid">
+        <Logo />
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={`/${route.APP.HOME}`}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={`/${route.APP.CUSTOMERS}`}>Clientes</NavLink>
+            </li>
+            <li>
+              <NavLink to={`/${route.APP.CONFIGURATIONS}`}>
+                Configurações
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Avatar />
+      </div>
     </div>
   );
 };
