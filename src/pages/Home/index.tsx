1829409813faff603e94da161c00a441d960e0e7
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-23 18:07:19 -0300

    feat: importando o banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
+import Banner from "components/Banner";
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <Link to="/customers">Customers</Link>
+      <Banner />
     </>
   );
 };
