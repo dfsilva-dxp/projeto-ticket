cdbf57c8dfc9d2df10699b389ead8a433f6dd5a6
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-25 15:43:44 -0300

    feat: recebendo as rotas do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
+import { AppRoutes } from "routes";
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
+    <Router>
+      <AppRoutes />
       <ToastContainer autoClose={3000} />
-    </>
+    </Router>
   );
 }
 
