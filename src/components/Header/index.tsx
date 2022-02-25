ec0599b171b3ba2d5f977ed25078913e4223b221
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-25 19:16:08 -0300

    feat: implementando o component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+import Avatar from "components/Avatar";
+import { route } from "constants/routes";
+
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="nav-content">
+        <Link to={route.APP.TICKETS}>
+          <img
+            src="./assets/logo.svg"
+            alt="Headset"
+            title="Headset amarelo que representa o logo do app"
+          />
+        </Link>
+        <Avatar
+          user={{
+            uid: "10",
+            name: "Daniel Silva",
+            email: "dfsilva.dxp@gmail.com",
+            photoURL: "https://avatars.githubusercontent.com/u/16245261?v=4",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
