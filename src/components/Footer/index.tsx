059abd367a383635640fafb68ee620edc28f8099
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-25 14:59:38 -0300

    feat: implementando o component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+    <footer className="login-footer">
+      <p>
+        &copy; 2022 &bull; <span>ticket.app</span> Todos os direitos reservados
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
