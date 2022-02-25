48ae5dbb5ebe9f572f7c0b414d14bdf534e91a84
Daniel Silva <dani3l.dxp@gmail.com>
2022-02-25 17:05:16 -0300

    feat: implementando a sidebar

---
+++ src/components/Navigation/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+import { RiAddBoxLine, RiFileListLine } from "react-icons/ri";
+import { TiPowerOutline, TiUserOutline } from "react-icons/ti";
+
+import NavSection from "../NavSection";
+import NavLinks from "../NavLinks";
+
+import "./styles.scss";
+import { route } from "constants/routes";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <NavSection title="Chamados">
+        <NavLinks
+          label="Todos"
+          href={route.APP.TICKETS}
+          icon={<RiFileListLine />}
+        />
+        <NavLinks
+          label="Abrir"
+          href={route.APP.TICKETS}
+          icon={<RiAddBoxLine />}
+        />
+      </NavSection>
+      <NavSection title="Clientes">
+        <NavLinks
+          label="Todos"
+          href={route.APP.CUSTOMERS}
+          icon={<RiFileListLine />}
+        />
+        <NavLinks
+          label="Novo"
+          href="/new-transaction"
+          icon={<RiAddBoxLine />}
+        />
+      </NavSection>
+      <NavSection title="Configurações">
+        <NavLinks
+          label="Perfil"
+          href={route.APP.PROFILE}
+          icon={<TiUserOutline />}
+        />
+        <span>
+          <TiPowerOutline /> Sair
+        </span>
+      </NavSection>
+    </div>
+  );
+};
