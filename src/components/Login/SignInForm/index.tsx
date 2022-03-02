306d1395587958717f2dc0340c607d8703c10c3b
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-02 09:01:39 -0300

    feat: fazendo login dos users cadastrados

--- src/components/Login/SignInForm/index.tsx
+++ src/components/Login/SignInForm/index.tsx
@@ -1,14 +1,30 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useRef } from "react";
 import { Link } from "react-router-dom";
 import { TiLockClosedOutline, TiMail } from "react-icons/ti";
 
 import { route } from "constants/routes";
+import useAuth from "hooks/useAuth";
 
 import { Head } from "components/Head";
 import { Input } from "components/Input";
 import Button from "components/Button";
 
 const SignInForm = () => {
+  const { authenticate } = useAuth();
+
+  const emailElRef = useRef<HTMLInputElement>(null);
+  const passwordElRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (emailElRef.current && passwordElRef.current) {
+      const email = emailElRef.current.value;
+      const password = passwordElRef.current.value;
+
+      authenticate({ email, password });
+    }
+  }
+
   return (
     <>
       <Head
@@ -25,7 +41,7 @@ const SignInForm = () => {
           </h1>
         </div>
 
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <h2>Log in</h2>
           <Input
             type="email"
@@ -34,6 +50,7 @@ const SignInForm = () => {
             required
             placeholder="Seu e-mail"
             icon={<TiMail />}
+            ref={emailElRef}
           />
           <Input
             type="password"
@@ -42,6 +59,7 @@ const SignInForm = () => {
             required
             placeholder="Sua senha"
             icon={<TiLockClosedOutline />}
+            ref={passwordElRef}
           />
           <Link to={`/${route.LOGIN.FORGOT}`}>Esqueceu a senha?</Link>
           <Button type="submit" size="full" btnStyle="square">
