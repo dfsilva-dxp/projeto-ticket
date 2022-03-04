ae2b36377d79a9740dcea577f5d4623d3d2363b3
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-04 13:43:20 -0300

    delete file

--- src/components/Jumbotron/index.tsx
+++
@@ -1,9 +0,0 @@
-import { JumbotronProps } from "./types";
-
-import "./styles.scss";
-
-const Jumbotron = ({ children }: JumbotronProps) => {
-  return <div className="jumbotron">{children}</div>;
-};
-
-export default Jumbotron;
