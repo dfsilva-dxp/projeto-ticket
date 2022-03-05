3d785e9cf12071d97e06724e438d7a3f1f3412c2
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-05 18:46:45 -0300

    feat: definindo os tipos de form

---
+++ src/components/Form/types.ts
@@ -0,0 +1,32 @@
+import {
+  ButtonHTMLAttributes,
+  FormHTMLAttributes,
+  HTMLAttributes,
+  ReactNode,
+} from "react";
+import { LinkProps } from "react-router-dom";
+
+export type FormsProps = {
+  children: ReactNode;
+} & HTMLAttributes<HTMLDivElement>;
+
+export type ContentProps = FormsProps;
+
+export type BaseProps = FormHTMLAttributes<HTMLFormElement> &
+  Pick<FormsProps, "children">;
+
+export type TitleProps = HTMLAttributes<HTMLHeadingElement> &
+  Pick<FormsProps, "children">;
+
+export type CustomLinkProps = LinkProps & Pick<FormsProps, "children">;
+
+export type TextProps = HTMLAttributes<HTMLParagraphElement> &
+  Pick<FormsProps, "children">;
+
+export type ButtonProps = {
+  size?: "small" | "medium" | "full";
+  color?: "default" | "white" | "gray";
+  btnStyle?: "square" | "round";
+  icon?: JSX.Element;
+} & ButtonHTMLAttributes<HTMLButtonElement> &
+  Pick<FormsProps, "children">;
