3e614b64d3102bf2fd983323f23c619711a6f4c4
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-06 07:22:13 -0300

    feat: export default signUp

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Display } from "./Display";
 export { default as Form } from "./Form";
 export { default as SignIn } from "./Login/signIn";
+export { default as SignUp } from "./Login/signUp";
