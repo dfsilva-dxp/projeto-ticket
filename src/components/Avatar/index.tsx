2f886cb2cfb92161c9718998f0acd8eed3625df4
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-06 10:20:49 -0300

    feat: definindo a estrutura do avatar component

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,28 +1,34 @@
-import { TiUserOutline } from "react-icons/ti";
+import {
+  AvatarProps,
+  ContentProps,
+  EmailProps,
+  NameProps,
+  PhotoProps,
+  WrapperProps,
+} from "./types";
 
-import { AvatarProps } from "./types";
+import { Container, Content, Email, Name, Photo, Wrapper } from "./styles";
 
-import "./styles.scss";
+export default function Avatar({ children }: AvatarProps) {
+  return <Container>{children}</Container>;
+}
 
-const Avatar = ({ user = null }: AvatarProps) => {
-  return (
-    <div className="avatar-wrapper">
-      <div
-        className="photo"
-        style={{
-          backgroundImage: !!user?.photoURL ? `url(${user.photoURL})` : "",
-        }}
-      >
-        {!!!user?.photoURL && <TiUserOutline />}
-      </div>
-      {user && (
-        <div className="user-info">
-          <h4>{user.displayName}</h4>
-          <small>{user.email}</small>
-        </div>
-      )}
-    </div>
-  );
+Avatar.Wrapper = function AvatarWrapper({ children }: WrapperProps) {
+  return <Wrapper>{children}</Wrapper>;
 };
 
-export default Avatar;
+Avatar.Photo = function AvatarPhoto({ children, photoURL = null }: PhotoProps) {
+  return <Photo BackgroundUrl={photoURL}>{!!!photoURL && children}</Photo>;
+};
+
+Avatar.Content = function AvatarContent({ children }: ContentProps) {
+  return <Content>{children}</Content>;
+};
+
+Avatar.Name = function AvatarName({ displayName }: NameProps) {
+  return <Name>{displayName}</Name>;
+};
+
+Avatar.Email = function AvatarEmail({ email }: EmailProps) {
+  return <Email>{email}</Email>;
+};
