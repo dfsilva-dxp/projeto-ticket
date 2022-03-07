a2db8b8efdd5c3dfddd094cc3a499a3c974a0095
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-07 16:48:59 -0300

    feat: export default footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as Avatar } from "./Avatar";
+export { default as Footer } from "./Footer";
 export { default as Display } from "./Display";
 export { default as Form } from "./Form";
 export { default as SignIn } from "./Login/signIn";
