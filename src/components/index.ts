5c4099bd75af2a298dabd53dce95df667605e51b
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 17:58:22 -0300

    feat: add export default nav

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { default as Head } from "./Head";
 export { default as Header } from "./Header";
 export { default as Input } from "./Input";
 export { default as Layout } from "./Layout";
+export { default as Nav } from "./Navigation";
