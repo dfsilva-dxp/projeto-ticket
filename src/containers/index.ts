cb647683f57aaace24aed863db8c092741c026c2
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 15:16:32 -0300

    feat: export default signIn e signUp

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
-export { default as FormContainer } from "./form";
 export { default as FooterContainer } from "./footer";
 export { default as HeaderContainer } from "./header";
+export { default as SignIn } from "./signIn";
+export { default as SignUp } from "./signUp";
