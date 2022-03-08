824689323fdee4db4807fdb7c5bc1d783b814880
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 14:40:45 -0300

    feat: migrando para nova estrutura

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,19 @@
-import { Link } from "react-router-dom";
+import { ContentProps, CustomLinkProps, HeaderProps } from "./types";
 
-import useAuth from "hooks/useAuth";
+import { Container, Content, Image, Link } from "./styles";
 
-import { route } from "constants/routes";
+export default function Header({ children }: HeaderProps) {
+  return <Container>{children}</Container>;
+}
 
-import "./styles.scss";
-import { Avatar } from "components";
-import { TiUserOutline } from "react-icons/ti";
-
-const Header = () => {
-  const { user } = useAuth();
-
-  return (
-    <div className="header">
-      <div className="nav-content">
-        <Link to={route.APP.CALLEDS.ALL}>
-          <img
-            src="./assets/logo.svg"
-            alt="Headset"
-            title="Headset amarelo que representa o logo do app"
-          />
-        </Link>
-        <Avatar>
-          <Avatar.Wrapper>
-            <Avatar.Photo photoURL={user?.photoURL}>
-              <TiUserOutline />
-            </Avatar.Photo>
-          </Avatar.Wrapper>
-          <Avatar.Content>
-            <Avatar.Name displayName={user?.displayName} />
-            <Avatar.Email email={user?.email} />
-          </Avatar.Content>
-        </Avatar>
-      </div>
-    </div>
-  );
+Header.Content = function HeaderContent({ children }: ContentProps) {
+  return <Content>{children}</Content>;
 };
 
-export default Header;
+Header.Link = function HeaderLink({ children, ...props }: CustomLinkProps) {
+  return <Link {...props}>{children}</Link>;
+};
+
+Header.Logo = function HeaderLogo({ ...props }) {
+  return <Image {...props} />;
+};
