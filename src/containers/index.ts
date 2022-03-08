35a3669aeed8eed9f51a112cd6ffc6bf3b773803
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 15:39:01 -0300

    feat: export default layout

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,5 @@
 export { default as FooterContainer } from "./footer";
 export { default as HeaderContainer } from "./header";
+export { default as Layout } from "./layout";
 export { default as SignIn } from "./signIn";
 export { default as SignUp } from "./signUp";
