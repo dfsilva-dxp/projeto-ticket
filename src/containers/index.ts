9b72ce20e1dc91d5b256c1812c4e3ecf490ab333
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 18:01:02 -0300

    feat: add export default sidebar

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,5 +1,6 @@
 export { default as FooterContainer } from "./footer";
 export { default as HeaderContainer } from "./header";
 export { default as Layout } from "./layout";
+export { default as Sidebar } from "./sidebar";
 export { default as SignIn } from "./signIn";
 export { default as SignUp } from "./signUp";
