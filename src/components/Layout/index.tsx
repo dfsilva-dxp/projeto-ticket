423577a1291373929f73eeff1ad12d41733adb8c
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-08 14:42:31 -0300

    feat: importando a nova estrutura do component header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import { Sidebar } from "components/Navigation/Sidebar";
-import Header from "components/Header";
 
 import "./styles.scss";
+import { HeaderContainer } from "containers";
 
 const Layout = () => {
   return (
     <>
-      <Header />
+      <HeaderContainer />
       <section className="wrapper-layout">
         <Sidebar />
         <div className="content">
