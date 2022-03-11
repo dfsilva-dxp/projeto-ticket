46882fad5d4681d71b64659367be843eae702f34
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-11 17:09:17 -0300

    feat: definindo os estilos do profile component

---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Jumbotron = styled.div`
+  height: table;
+  background-color: var(--gray-800);
+  padding: 2.5rem;
+  border-radius: 0.5rem;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+`;
+
+export const PhotoContent = styled.div``;
+export const FormContent = styled.div``;
