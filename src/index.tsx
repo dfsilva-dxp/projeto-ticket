0d3b22062c32fd694724dbaa2ede81bb1ed4176b
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-11 14:05:29 -0300

    feat: delete import dos estilos

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "styles/global.scss";
-import { GlobalStyles } from "styles/global-styles";
-
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
