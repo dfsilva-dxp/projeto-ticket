d1d622ebd57bbb65bd7cc96b230890b6dd81b5bf
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-11 17:59:34 -0300

    feat: add export default profileContainer

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,7 @@
 export { default as FooterContainer } from "./footer";
 export { default as HeaderContainer } from "./header";
 export { default as Layout } from "./layout";
+export { default as ProfileContainer } from "./profile";
 export { default as Sidebar } from "./sidebar";
 export { default as SignIn } from "./signIn";
 export { default as SignUp } from "./signUp";
