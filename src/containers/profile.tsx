8b7df4b85820e3a60f663c8d27c98c12396d15c1
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-16 16:13:00 -0300

    feat: refatorando inputs

--- src/containers/profile.tsx
+++ src/containers/profile.tsx
@@ -3,13 +3,27 @@ import { TiMail, TiUserOutline } from "react-icons/ti";
 import { Avatar, Form, Input, PageTitle, Profile } from "components";
 
 import useFirebase from "hooks/useFirebase";
+import { FormEvent, useRef } from "react";
+import useProfile from "hooks/useProfile";
 
 type ProfileContainerProps = {
   title: string;
 };
 
 export default function ProfileContainer({ title }: ProfileContainerProps) {
+  const nameElRef = useRef<HTMLInputElement>(null);
+
   const { user } = useFirebase();
+  const { url, loading, save, changeFileAndUrl } = useProfile();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (user) {
+      const { uid } = user;
+      save(uid, nameElRef.current!.value);
+    }
+  }
 
   return (
     <>
@@ -21,10 +35,16 @@ export default function ProfileContainer({ title }: ProfileContainerProps) {
             <Avatar.Wrapper size="large">
               <Avatar.Editor>
                 <span>editar foto</span>
-                <Avatar.File type="file" accept="image/*" />
+                <Avatar.File
+                  type="file"
+                  accept="image/*"
+                  onChange={changeFileAndUrl}
+                />
               </Avatar.Editor>
 
-              <Avatar.Photo photoURL={user?.photoURL}>
+              <Avatar.Photo
+                photoURL={!!url ? url : !!user?.photoURL ? user.photoURL : ""}
+              >
                 <TiUserOutline />
               </Avatar.Photo>
             </Avatar.Wrapper>
@@ -32,29 +52,33 @@ export default function ProfileContainer({ title }: ProfileContainerProps) {
         </Profile.Photo>
 
         <Profile.Form>
-          <Form.Base>
+          <Form.Base onSubmit={handleSubmit}>
             <Form.Inline>
               <Input
                 type="text"
                 name="name"
                 id="name"
+                defaultValue={user?.displayName as string}
                 required
                 placeholder="Seu nome"
                 icon={<TiUserOutline />}
+                ref={nameElRef}
               />
 
               <Input
                 type="email"
                 name="email"
                 id="email"
+                defaultValue={user?.email as string}
+                readOnly
                 required
                 placeholder="Seu e-mail"
                 icon={<TiMail />}
               />
             </Form.Inline>
 
-            <Form.Button type="submit" btnStyle="square">
-              Salvar
+            <Form.Button type="submit" btnStyle="square" disabled={loading}>
+              {loading ? "Loading..." : "Salvar"}
             </Form.Button>
           </Form.Base>
         </Profile.Form>
